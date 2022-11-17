f9584f7c4b94a2e3918627074b9cb0d0ef1ee6ea
CPLepage <lepage.charlespilippe@gmail.com>
2022-11-17 09:09:03 -0500

    windows getting in shape

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,17 +3,47 @@
 // Feel free to use any frontend framework you like :)
 // See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
 
+let commands;
+async function initCommands(){
+    commands = {
+        checkVersion: {
+            Darwin: "deno --version",
+            Windows: "deno --version"
+        },
+        downloadAndInstall: {
+            Darwin: "curl -fsSL https://deno.land/x/install/install.sh | sh",
+            Windows: "powershell -command \"irm https://deno.land/install.ps1 | iex\""
+        },
+        postInstall: {
+            Darwin: "echo export DENO_INSTALL=\"/Users/" + await Neutralino.os.getEnv('USER') + "/.deno\" >> $HOME/.zshrc && echo export PATH=\"\\$DENO_INSTALL/bin:\\$PATH\" >> $HOME/.zshrc",
+            Windows: "setx /M path \"%path%;C:\\Users\\" +  await Neutralino.os.getEnv('USER') + "\\.deno\\bin\\deno.exe\""
+        }
+    }
+}
+
+function getCommand(cmd){
+    const osCommands = commands[cmd];
+
+    if(!osCommands)
+        throw Error(`Unknown command: ${cmd}`);
+
+    const specificCommand = osCommands[NL_OS];
+
+    if(!specificCommand)
+        throw Error(`Command not implemented for OS: ${cmd} [${NL_OS}]`);
+
+    return specificCommand;
+}
+
 async function showInfo() {
     fetch("https://api.github.com/repos/denoland/deno/releases/latest")
         .then(res => res.json())
         .then(latestRelease => {
             document.querySelector("#latest-version").innerHTML = `Latest Version: ${latestRelease.tag_name}`;
-        })
+        });
 
-    const test = await Neutralino.os.execCommand("source ~/.zshrc && deno --version", {});
+    const test = await Neutralino.os.execCommand(getCommand("checkVersion"), {});
     console.log(test)
-    console.log(await Neutralino.os.execCommand(" echo $PATH", {}));
-    console.log(await Neutralino.os.execCommand("echo $USER", {}));
     document.getElementById("is-install").innerHTML = test.stdOut
         ? "✅"
         : "❌";
@@ -21,9 +51,9 @@ async function showInfo() {
     document.querySelector("#version").innerHTML = `<div style="text-align: left; margin: 0 auto; max-width: 400px; margin-top: 24px">Installed Version: <pre>${test.stdOut}</pre></div>`
 }
 
-function darwinDenoInstall(){
+function denoInstall(){
     return new Promise(async resolve => {
-        let nodeProc = await Neutralino.os.spawnProcess("curl -fsSL https://deno.land/x/install/install.sh | sh");
+        let nodeProc = await Neutralino.os.spawnProcess(getCommand("downloadAndInstall"));
 
         Neutralino.events.on('spawnedProcess', (evt) => {
             if(nodeProc.id === evt.detail.id) {
@@ -43,19 +73,27 @@ function darwinDenoInstall(){
     })
 }
 
-async function installDeno(){
-    console.log("Launching Deno Installation");
-    await darwinDenoInstall();
-
-    const zshrcContent = (await Neutralino.os.execCommand(`cat $HOME/.zshrc`, {})).stdOut;
-    if(!zshrcContent.match(/DENO_INSTALL=/g)){
-        const command = `echo export DENO_INSTALL="/Users/${await Neutralino.os.getEnv('USER')}/.deno" >> $HOME/.zshrc`;
-        const command2 = `echo export PATH="\\$DENO_INSTALL/bin:\\$PATH" >> $HOME/.zshrc`;
-        await Neutralino.os.execCommand(command, {});
-        await Neutralino.os.execCommand(command2, {});
+async function shouldProcessWithPostInstall(){
+    if(NL_OS === "Darwin") {
+        const zshrcContent = (await Neutralino.os.execCommand(`cat $HOME/.zshrc`, {})).stdOut;
+        return !zshrcContent.match(/DENO_INSTALL=/g);
+    }else if(NL_OS === "Windows"){
+        const pathContent = (await Neutralino.os.execCommand(`path`, {})).stdOut;
+        return !pathContent.match(/bin\\deno.exe/g)
     }
 
-    await Neutralino.app.restartProcess({});
+    return false;
+}
+
+async function installDeno(){
+    console.log("Launching Deno Installation");
+    await denoInstall();
+
+    if(await shouldProcessWithPostInstall()) {
+        await Neutralino.os.execCommand(getCommand("postInstall"), {});
+    }
+
+    await Neutralino.app.exit(0);
 }
 
 function openDocs() {
@@ -108,4 +146,4 @@ if(NL_OS != "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinoj
     setTray();
 }
 
-showInfo();
+initCommands().then(showInfo)

aafb7f8c5f79b1984a9188db85ac58261325e729
cplepage <lepage.charlesphilippe@gmail.com>
2022-11-16 22:55:28 -0500

    getting in shape

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,12 +4,21 @@
 // See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
 
 async function showInfo() {
-    const test = await Neutralino.os.execCommand("deno --version", {});
+    fetch("https://api.github.com/repos/denoland/deno/releases/latest")
+        .then(res => res.json())
+        .then(latestRelease => {
+            document.querySelector("#latest-version").innerHTML = `Latest Version: ${latestRelease.tag_name}`;
+        })
+
+    const test = await Neutralino.os.execCommand("source ~/.zshrc && deno --version", {});
+    console.log(test)
+    console.log(await Neutralino.os.execCommand(" echo $PATH", {}));
+    console.log(await Neutralino.os.execCommand("echo $USER", {}));
     document.getElementById("is-install").innerHTML = test.stdOut
         ? "✅"
         : "❌";
 
-    document.body.innerHTML += `<div style="text-align: left; margin: 0 auto; max-width: 400px; margin-top: 24px">Currrent Version: <pre>${test.stdOut}</pre></div>`
+    document.querySelector("#version").innerHTML = `<div style="text-align: left; margin: 0 auto; max-width: 400px; margin-top: 24px">Installed Version: <pre>${test.stdOut}</pre></div>`
 }
 
 function darwinDenoInstall(){
@@ -20,10 +29,10 @@ function darwinDenoInstall(){
             if(nodeProc.id === evt.detail.id) {
                 switch(evt.detail.action) {
                     case 'stdOut':
-                        console.log(evt.detail.data); // 10
+                        document.querySelector("#logs").innerHTML += evt.detail.data;
                         break;
                     case 'stdErr':
-                        console.error(evt.detail.data);
+                        document.querySelector("#logs").innerHTML += evt.detail.data;
                         break;
                     case 'exit':
                         resolve();
@@ -37,10 +46,16 @@ function darwinDenoInstall(){
 async function installDeno(){
     console.log("Launching Deno Installation");
     await darwinDenoInstall();
-    const command = `echo export DENO_INSTALL="/Users/${await Neutralino.os.getEnv('USER')}/.deno" >> $HOME/.zshrc`;
-    const command2 = `echo export PATH="\\$DENO_INSTALL/bin:\\$PATH" >> $HOME/.zshrc`;
-    await Neutralino.os.execCommand(command, {});
-    await Neutralino.os.execCommand(command2, {});
+
+    const zshrcContent = (await Neutralino.os.execCommand(`cat $HOME/.zshrc`, {})).stdOut;
+    if(!zshrcContent.match(/DENO_INSTALL=/g)){
+        const command = `echo export DENO_INSTALL="/Users/${await Neutralino.os.getEnv('USER')}/.deno" >> $HOME/.zshrc`;
+        const command2 = `echo export PATH="\\$DENO_INSTALL/bin:\\$PATH" >> $HOME/.zshrc`;
+        await Neutralino.os.execCommand(command, {});
+        await Neutralino.os.execCommand(command2, {});
+    }
+
+    await Neutralino.app.restartProcess({});
 }
 
 function openDocs() {
